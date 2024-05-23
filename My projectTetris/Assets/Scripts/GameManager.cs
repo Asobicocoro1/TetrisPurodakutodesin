7f2fd43e1384130e560789f11ba3cb18d3e5a5b8
Asobicocoro1 <amoruhu3@gmail.com>
2024-05-23 17:57:57 +0900

    ゲームオーバーの仕様を追加
    グリッドラインがビルドしたときに表示されないのでいつかやる

--- My projectTetris/Assets/Scripts/GameManager.cs
+++ My projectTetris/Assets/Scripts/GameManager.cs
@@ -1,17 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    // ゲームフィールドの幅
     public static int width = 10;
+    // ゲームフィールドの高さ
     public static int height = 20;
+    // ゲームフィールドの各位置にあるブロックを管理するための配列
     public Transform[,] grid = new Transform[width, height];
 
+    // テトリミノのプレハブ（ブロックのテンプレート）の配列
     public GameObject[] tetrominoes;
+    // ゲームオーバー時に表示するテキスト
+    public Text gameOverText;
 
+    // GameManagerのインスタンスを保持するための変数
     public static GameManager instance;
+
+    // ゲームオーバー状態を示すフラグ
+    private bool isGameOver = false;
 
     private void Awake()
     {
+        // インスタンスがまだ存在しない場合は設定し、存在する場合は削除
         if (instance == null)
         {
             instance = this;
@@ -26,32 +38,53 @@ public class GameManager : MonoBehaviour
     {
         // フレームレートを60に固定
         Application.targetFrameRate = 60;
-        // テトリミノを生成
+        // 最初のテトリミノを生成
         SpawnTetromino();
     }
 
+    // 新しいテトリミノを生成するメソッド
     public void SpawnTetromino()
     {
+        // ゲームオーバー状態ならブロックを生成しない
+        if (isGameOver)
+        {
+            return;
+        }
+
+        // 一番上の行にブロックがあるかチェック
+        if (IsGameOver())
+        {
+            GameOver();
+            return;
+        }
+
+        // テトリミノをランダムに選んで生成
         int index = Random.Range(0, tetrominoes.Length);
         Instantiate(tetrominoes[index], new Vector3(width / 2, height, 0), Quaternion.identity);
     }
 
+    // 指定された位置がゲームフィールド内にあるかどうかを確認するメソッド
     public bool IsInsideGrid(Vector2 pos)
     {
+        // xが0以上width未満、かつyが0以上である場合
         return ((int)pos.x >= 0 && (int)pos.x < width && (int)pos.y >= 0);
     }
 
+    // 座標を整数に丸めるメソッド
     public Vector2 Round(Vector2 pos)
     {
         return new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y));
     }
 
+    // 指定された位置にブロックがあるかどうかを取得するメソッド
     public Transform GetTransformAtGridPosition(Vector2 pos)
     {
+        // yがフィールドの高さより大きい場合はnullを返す
         if (pos.y > height - 1) return null;
         return grid[(int)pos.x, (int)pos.y];
     }
 
+    // ラインが揃っているか確認し、揃っていれば削除するメソッド
     public void CheckForLines()
     {
         for (int y = 0; y < height; y++)
@@ -65,6 +98,7 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    // 指定された行が満杯かどうかを確認するメソッド
     bool IsFullLineAt(int y)
     {
         for (int x = 0; x < width; x++)
@@ -77,6 +111,7 @@ public class GameManager : MonoBehaviour
         return true;
     }
 
+    // 指定された行を削除するメソッド
     void DeleteLine(int y)
     {
         for (int x = 0; x < width; x++)
@@ -86,6 +121,7 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    // 指定された行から上の全ての行を1つ下に移動するメソッド
     void MoveAllRowsDown(int startY)
     {
         for (int y = startY; y < height; y++)
@@ -94,6 +130,7 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    // 指定された行を1つ下に移動するメソッド
     void MoveRowDown(int y)
     {
         for (int x = 0; x < width; x++)
@@ -105,5 +142,28 @@ public class GameManager : MonoBehaviour
                 grid[x, y - 1].position += Vector3.down;
             }
         }
+    }
+
+    // 一番上の行にブロックがあるかどうかを確認するメソッド
+    bool IsGameOver()
+    {
+        // 一番上の行にブロックがあるかチェック
+        for (int x = 0; x < width; x++)
+        {
+            if (grid[x, height - 1] != null)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // ゲームオーバー処理を行うメソッド
+    void GameOver()
+    {
+        // ゲームオーバーテキストを表示
+        gameOverText.gameObject.SetActive(true);
+        // ゲームオーバーフラグを設定
+        isGameOver = true;
     }
 }
--- My projectTetris/Assets/Scripts/Tetromino.cs
+++ My projectTetris/Assets/Scripts/Tetromino.cs
@@ -2,82 +2,115 @@
 
 public class Tetromino : MonoBehaviour
 {
+    // 通常の落下速度
     public float fallSpeed = 1.0f;
+    // 前回の落下時間
     private float fallTime = 0;
+    // 高速落下の倍率
     private float fastFallMultiplier = 10.0f;
 
     void Update()
     {
+        // 毎フレームごとに移動、回転、落下の処理を行う
         Move();
         Rotate();
         Fall();
     }
 
+    // テトリミノの移動処理
     void Move()
     {
+        // 左矢印キーが押されたとき
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
+            // テトリミノを左に移動
             transform.position += Vector3.left;
+            // 移動後の位置が有効か確認
             if (!IsValidPosition())
             {
+                // 無効な場合は元の位置に戻す
                 transform.position += Vector3.right;
             }
         }
+        // 右矢印キーが押されたとき
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
+            // テトリミノを右に移動
             transform.position += Vector3.right;
+            // 移動後の位置が有効か確認
             if (!IsValidPosition())
             {
+                // 無効な場合は元の位置に戻す
                 transform.position += Vector3.left;
             }
         }
     }
 
+    // テトリミノの回転処理
     void Rotate()
     {
+        // 上矢印キーが押されたとき
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
+            // テトリミノを90度回転
             transform.Rotate(0, 0, 90);
+            // 回転後の位置が有効か確認
             if (!IsValidPosition())
             {
+                // 無効な場合は元の位置に戻す
                 transform.Rotate(0, 0, -90);
             }
         }
     }
 
+    // テトリミノの落下処理
     void Fall()
     {
         float currentFallSpeed = fallSpeed;
 
+        // 下矢印キーが押されているときは落下速度を速くする
         if (Input.GetKey(KeyCode.DownArrow))
         {
             currentFallSpeed /= fastFallMultiplier;
         }
 
+        // 前回の落下時間から現在の時間を引いた値が現在の落下速度以上の場合
         if (Time.time - fallTime >= currentFallSpeed)
         {
+            // テトリミノを下に移動
             transform.position += Vector3.down;
+            // 落下時間を現在の時間に更新
             fallTime = Time.time;
+            // 移動後の位置が有効か確認
             if (!IsValidPosition())
             {
+                // 無効な場合は元の位置に戻す
                 transform.position += Vector3.up;
+                // グリッドにテトリミノを追加
                 AddToGrid();
+                // ラインが揃っているか確認し、揃っている場合は削除
                 FindObjectOfType<GameManager>().CheckForLines();
+                // 新しいテトリミノを生成
                 FindObjectOfType<GameManager>().SpawnTetromino();
+                // 現在のテトリミノを無効にする
                 enabled = false;
             }
         }
     }
 
+    // テトリミノの位置が有効か確認するメソッド
     bool IsValidPosition()
     {
+        // テトリミノの各ブロックの位置を確認
         foreach (Transform child in transform)
         {
             Vector2 pos = FindObjectOfType<GameManager>().Round(child.position);
+            // ブロックの位置がグリッド内か確認
             if (!FindObjectOfType<GameManager>().IsInsideGrid(pos))
             {
                 return false;
             }
+            // その位置に既にブロックがあるか確認
             if (FindObjectOfType<GameManager>().GetTransformAtGridPosition(pos) != null)
             {
                 return false;
@@ -86,12 +119,18 @@ public class Tetromino : MonoBehaviour
         return true;
     }
 
+    // テトリミノをグリッドに追加するメソッド
     void AddToGrid()
     {
+        // テトリミノの各ブロックをグリッドに追加
         foreach (Transform child in transform)
         {
             Vector2 pos = FindObjectOfType<GameManager>().Round(child.position);
-            FindObjectOfType<GameManager>().grid[(int)pos.x, (int)pos.y] = child;
+            // グリッドの範囲外に追加しないようにチェック
+            if ((int)pos.x >= 0 && (int)pos.x < GameManager.width && (int)pos.y >= 0 && (int)pos.y < GameManager.height)
+            {
+                FindObjectOfType<GameManager>().grid[(int)pos.x, (int)pos.y] = child;
+            }
         }
     }
 }
